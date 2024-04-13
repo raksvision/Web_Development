2c8b5c1fbcea01b4911d8000f6ea1fbfed9d8d30
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-13 23:29:11 +0530

    added operator file

--- JavaScript/lecture41/operators.js
+++ JavaScript/lecture41/operators.js
@@ -1,5 +1,5 @@
 // arithmetic operator
-
+/* Binary Operrator*/
 let a = 10;
 let b = 3;
 
@@ -8,3 +8,32 @@ console.log("Subratction: ",a-b); //7
 console.log("Multiplication: ", a*b); //30
 console.log("Division: ", a/b); //3.333333333
 console.log("Modulus: ", a%b); //1
+
+/* Unary Operrator*/
+
+let x = 5;
+
+console.log(++x); // preIncrement
+console.log(a++); // postIncrement
+
+console.log(a) //a = 11
+
+console.log(--b); // preDecrement
+console.log(b--); // postDecrement
+
+console.log(b); // b = 1
+
+
+//<------------------------------------------------>
+
+// assignment operator
+
+let a1 = 14;
+
+console.log(a1 += 5); //a1 = 19 
+console.log(a1 -= 5); //a1 = 9
+console.log(a1 *= 2); //a1 = 28
+console.log(a1 /= 5); //a1 = 2.8
+
+//<------------------------------------------------>
+
