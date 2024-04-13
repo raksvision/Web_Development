db9b9661eb1616a80d20c32c37f9a9bc421ac1d8
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-14 00:48:07 +0530

    added operator file

--- JavaScript/lecture41/operators.js
+++ JavaScript/lecture41/operators.js
@@ -55,5 +55,84 @@ console.log(10 >= 5); //true
 console.log(10 <= 5); //false
 console.log(4 == 5); //false
 console.log(4 != 5); //true
+console.log(5 !== 5); //false
+
+//<------------------------------------------------>
+
+/* Ternary operator */
+
+let age = 23;
+let status1 = (age > 18) ? "I can Vote" : "I cannot Vote" ;
+             //condition      //true         //false
+console.log(status1); // I can Vote
+
+let price = 180;
+let wallet = (price > 200) ? "You can Buy" : "You cannot Buy";
+console.log(wallet); //You cannot Buy
+
+let myage = 21;
+
+(myage >= 18) ? (console.log("Can Drive")) : (console.log("Cannot Drive"));
+
+// Output : Can Drive
+
+//<------------------------------------------------>
+
+/* Logical operator */
+
+//AND(&&) *all the conditions must be true*
+console.log(true && false && true); //false
+console.log(false && false && false); //false
+console.log(true && true && true); //true
+
+//OR(||) *any one condition might be  true*
+console.log(true || false || false); //true
+console.log(false || false || false); //false
+console.log(false || false || true); //true
+
+//NOT(~)
+
+console.log(!true); //false
+console.log(!false); //true
+
+//<------------------------------------------------>
+
+/* Bitwise operator */ 
+
+// Bitwisw AND(&)
+
+console.log(2 & 5); //0
+console.log(5 & 7); //5
+console.log(5 & 3); //1
+
+//Bitwise OR(|)
+
+console.log(2 | 5); //7
+console.log(2 | 3); //3
+console.log(1 | 7); //7
+
+// Bitwise NOT(~)
+
+console.log(~0); // -1
+console.log(~(-5)) // 4
+
+//Bitwise XOR(^)
+
+console.log(2 ^ 2); // 0
+console.log(3 ^ 1); // 2
+
+//leftShift (<<) *multiply no.of times with '2'*
+
+console.log(5 << 1); // 10 (5*2)
+console.log(7 << 3); // 56 (7*2*2*2)
+console.log(1 << 2); //4 (1*2*2)
+
+//rightShift (>>) *divide no.of times with '2'*
+
+console.log(8 >> 2); // 4 (8/2**2)
+console.log(15 >> 2); // 3(15/2**2)
+console.log(7 >> 1); // 3(7/2**1)
+
+
 
 //<------------------------------------------------>
