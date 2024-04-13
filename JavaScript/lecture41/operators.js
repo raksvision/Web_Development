12dbc6a1cec1ad579448cf83f83427fb08ff4243
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-13 23:04:10 +0530

    added operator file

---
+++ JavaScript/lecture41/operators.js
@@ -0,0 +1,10 @@
+// arithmetic operator
+
+let a = 10;
+let b = 3;
+
+console.log("Addition: ", a+b); //13
+console.log("Subratction: ",a-b); //7
+console.log("Multiplication: ", a*b); //30
+console.log("Division: ", a/b); //3.333333333
+console.log("Modulus: ", a%b); //1
