c7bea819850129bed012388f5dddf65d53fa3492
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-14 10:21:22 +0530

    added conditionals file

---
+++ JavaScript/lecture41/conditionals.js
@@ -0,0 +1,24 @@
+/* simple if-statement */
+
+let age = 25;
+if(age >=18){
+    console.log("You can Vote");
+}
+
+let str = "Rakesh";
+if(str.length >= 3){
+    console.log("Your name " + str + " is Perfect.");
+}
+
+//<---------------------------------------------------->
+
+/* if-else statement */
+
+if(age >= 18){
+    console.log("You can Vote");
+}
+else{
+    console.log("You can'nt Vote");
+}
+
+//<---------------------------------------------------->
