dcc3578c5ef714b6c5ce8fb86813b7e6a7776bb7
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-14 10:33:48 +0530

    added conditionals file

--- JavaScript/lecture41/conditionals.js
+++ JavaScript/lecture41/conditionals.js
@@ -21,4 +21,52 @@ else{
     console.log("You can'nt Vote");
 }
 
+let marks = 35;
+if(marks >= 25){
+    console.log("Pass")
+}
+else{
+    console.log("Fail")
+}
+
+//<---------------------------------------------------->\
+
+/* nested if statement */
+
+let num = 3;
+
+if(num == 1){
+    console.log("A")
+}
+else if(num == 2){
+    console.log("B")
+}
+else if(num == 3){
+    console.log("C")
+}
+else if(num == 4){
+    console.log("D")
+}
+else if(num == 5){
+    console.log("E")
+}
+else if(num == 6){
+    console.log("F")
+}
+else{
+    console.log("None")
+}
+
+let weight = 70;
+
+if(weight > 75){
+    console.log("You are Overweight");
+}
+else if(weight>50 && weight <= 75){
+    console.log("You are Fit");
+}
+else{
+    console.log("You are Underweight")
+}
 //<---------------------------------------------------->
+
