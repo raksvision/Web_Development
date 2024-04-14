1923fb590e900ccebe02326d1cd6b8cbd96e7f55
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-14 10:44:16 +0530

    updated conditionals file

--- JavaScript/lecture41/conditionals.js
+++ JavaScript/lecture41/conditionals.js
@@ -70,3 +70,22 @@ else{
 }
 //<---------------------------------------------------->
 
+/* switch statement */
+
+let num1 = 5;
+
+switch(num1){
+    case 1 : console.log("A");
+    break;
+    case 2 : console.log("B");
+    break;
+    case 3 : console.log("C");
+    break;
+    case 4 : console.log("D");
+    break;
+    case 5 : console.log("E");
+    break;
+    default : console.log("None");
+}
+
+//<---------------------------------------------------->
