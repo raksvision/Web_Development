211ae97b5c18cc1c75b8751f78402f4df8e79592
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-16 23:58:24 +0530

    added strings file

---
+++ JavaScript/lecture42/strings.js
@@ -0,0 +1,20 @@
+/* strings */
+
+let firstName = "Rakesh";
+let lastName = "Bestha";
+
+console.log(firstName + lastName); //RakeshBestha
+console.log(typeof(firstName));//string
+console.log(typeof(lastName));//string
+
+//<------------------------------->
+// to print multiple line, we use backticks(``)
+
+let desc = `Here is 
+the proper description
+of the code.`
+
+console.log(desc);//it prints as you type in your code editor
+console.log(typeof(desc));//string
+
+//<------------------------>
