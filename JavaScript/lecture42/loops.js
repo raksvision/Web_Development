0b509953b04691f8fec2c51f7bf305a1a7a292fe
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-16 22:20:24 +0530

    added loops file

---
+++ JavaScript/lecture42/loops.js
@@ -0,0 +1,21 @@
+/* for loop */
+
+for(let i=1; i<=10;i++){
+    console.log("Rakesh");
+}//prints Rakesh 10 times
+
+//<------------------------->
+//counting number
+
+for (let i=1;i<=5;i++){
+    console.log(i);
+}//prints 1,2,3,4,5
+
+//<------------------------->
+//reverse counting
+
+for (let i=5; i>0 ;i--){
+    console.log(i);
+}//prints 5,4,3,2,1
+
+//<------------------------->
