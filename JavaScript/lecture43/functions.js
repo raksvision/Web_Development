f53e7bef791eb7f3604b88e8b27dc1ac18050584
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-17 23:04:37 +0530

    updated functions file

---
+++ JavaScript/lecture43/functions.js
@@ -0,0 +1,19 @@
+/* functions */
+
+//function definition
+function sayMyName(){
+    console.log("Rakesh");
+}
+
+//function use - function call
+sayMyName(); // Rakesh
+
+//conting numbers
+
+function counting(){
+    for (let i=1;i<=100;i++){
+        console.log(i);
+    }
+}
+
+counting();//Output : prints numberr from 1 - 100
