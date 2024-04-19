fb25244bf1ebedf355243b4250911aae3a996c97
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-19 22:24:03 +0530

    added arrays file

---
+++ JavaScript/lecture44/arrays.js
@@ -0,0 +1,13 @@
+let obj = {
+    name : "Rakesh",
+    age : 23,
+    weight : 78,
+    lastName : "Bestha",
+    work : function(){
+        console.log("Iam Going to Work");
+    }
+
+}
+
+console.log(obj);
+obj.work();
