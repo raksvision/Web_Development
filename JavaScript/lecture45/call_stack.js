77740838ddbdefdb301b96f19ce20d3b04427d26
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-24 23:45:28 +0530

    updated call_stack file

--- JavaScript/lecture45/call_stack.js
+++ JavaScript/lecture45/call_stack.js
@@ -17,3 +17,46 @@ Hello by f1!
 Hello by f2!
 
 */
+
+
+//Function call stack//
+
+//1.assign as a variable:
+
+let greet = function(){
+    console.log("Hello EveryOne");
+}
+greet();//Hello EveryOne
+
+//<------------------------------>
+
+//2.as a argument:
+
+function greetMe(say,fullName){
+    console.log("Hello ", fullName);
+    say();
+}
+
+function say(){
+    console.log("Hi!");
+}
+
+greetMe(say,"Rakesh") //execution starts from here..
+
+// Hello Rakesh
+// Hi!
+
+//3. return function
+
+function solve(){
+    return function(num){
+        return num * num;
+    }
+}
+
+let ans = solve(5); //here it enter to the return, saves the variable into the 52 line function 
+console.log(ans(10)) //then, here it enter into another return statement and gives the final answer.
+
+//100
+//<-------------------------->
+
