18ff3ccd6017d59f3373476e0342a9f6cac02c91
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-25 22:42:44 +0530

    added file

---
+++ JavaScript/lecture46/variable_scoping.js
@@ -0,0 +1,28 @@
+//Global Value 
+
+var age = 23;
+
+
+console.log(age);
+
+{
+    console.log(age);
+
+}
+
+if(true){
+    console.log(age);
+}
+
+for (let i=0; i<=2; i++){
+    console.log(age);
+}
+
+function sayHello(){
+    console.log("Hello ", age );
+}
+
+sayHello();
+
+
+
