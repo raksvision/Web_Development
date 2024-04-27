2210add341a1c0de80a6be14014515c6fc87ebdb
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-27 21:47:00 +0530

    added class

---
+++ JavaScript/lecture47/class.js
@@ -0,0 +1,18 @@
+class Human{
+    name = "Rakesh";
+    age = 23;
+    weight = 80;
+    height = 180;
+
+walking(){
+    console.log("I am Walking");
+}
+
+running(){
+    console.log("I am Running");
+}
+
+}
+
+let a = new Human;
+console.log(a.name);
