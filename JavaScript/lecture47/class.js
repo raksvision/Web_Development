b852bee10aa8b48147835a76112a93e37a495a98
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-04-29 15:13:42 +0530

    added get & set

--- JavaScript/lecture47/class.js
+++ JavaScript/lecture47/class.js
@@ -1,13 +1,16 @@
 class Human{
-    name = "Rakesh";
+    //propreties
+    #name = "Rakesh";
     age = 23;
     weight = 80;
     height = 180;
-
+    //functionalities
 walking(){
     console.log("I am Walking");
 }
-
+call(){
+    console.log("Hello",this.#name)
+}
 running(){
     console.log("I am Running");
 }
@@ -15,4 +18,32 @@ running(){
 }
 
 let a = new Human;
-console.log(a.name);
+console.log(a.age);
+a.call();//Hello Rakesh
+//console.log(a.#name);//private field can't access outside the class
+console.log("------------------------------------------");
+//<--------------------------->
+
+/* getter & setter in class*/
+
+class Student{
+    firstName = "Rakesh";
+
+    get getName(){
+        return this.firstName;
+    }
+
+    set setName(newName){
+        this.firstName = newName;
+    }
+}
+
+let student = new Student();
+
+console.log(student.firstName);//Rakesh
+
+console.log(student.getName);//Rakesh
+
+student.setName = "Sainath";
+
+console.log(student.firstName);//Sainath
