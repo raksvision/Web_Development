7eedc0f79ab9546b56d1ce7456d729a93bfbc2bc
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-05-01 22:44:35 +0530

    added error handling file

--- JavaScript/lecture50/errorHandling.js
+++ JavaScript/lecture50/errorHandling.js
@@ -22,8 +22,41 @@ try{
     console.log("Block ends here");
 }
 catch(e){
+    //custom error
     console.log("Now Inside the Catch block");
     console.log("The Error is:", e);//Reference Error
 }
 */
 
+
+//Final block; it will run everytime even the error is occured
+
+
+try{
+    console.log("Try Block Start");
+    console.log(x);
+    console.log("Try Block End");
+}
+
+catch(e){
+    console.log("Enters Catch block");
+    console.log("The Error is:", e);
+}
+
+finally{
+    console.log("It is the final block, it will print in any situation");
+};//it will print after error throw
+
+//<----------------->
+
+//throw in error handling: for custom errors
+
+
+try
+{
+ console.log(value);
+}
+
+catch(e){
+    throw new Error("Declare the Value First")
+}//Error: Declare the Value First
