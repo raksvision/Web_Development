fa91d3dfae9de73ca5453ca19842dab707bd6e0d
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-05-01 22:24:44 +0530

    added error handling file

--- JavaScript/lecture50/errorHandling.js
+++ JavaScript/lecture50/errorHandling.js
@@ -13,3 +13,17 @@ console.log(x);
 ReferenceError: x is not defined
 */
 //<-------------------->
+
+//Handling
+/*
+try{
+    console.log("Block Start Here");
+    console.log(x);
+    console.log("Block ends here");
+}
+catch(e){
+    console.log("Now Inside the Catch block");
+    console.log("The Error is:", e);//Reference Error
+}
+*/
+
