4a3b3b441cc23d8bad99b23e7d0e00f0eb93b729
Rakesh Bestha <b.rakesh3110@gmail.com>
2024-05-01 22:14:21 +0530

    added error handling file

---
+++ JavaScript/lecture50/errorHandling.js
@@ -0,0 +1,15 @@
+//Error is an event that impacts or disrepts the flow of execution of the code.
+/*
+compile time error
+console.log("Rakesh";
+SyntaxError
+
+
+
+runtime error
+
+console.log(x);
+
+ReferenceError: x is not defined
+*/
+//<-------------------->
